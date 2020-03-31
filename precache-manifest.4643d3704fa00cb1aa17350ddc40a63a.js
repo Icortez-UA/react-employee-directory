6c8d821648883ba293010a64f5ba2a4f3cd06279
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-03-31 15:03:41 -0700

    Updates

---
+++ precache-manifest.4643d3704fa00cb1aa17350ddc40a63a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c5d690815200e42caef36a95604683",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "01ae29b8ddb8de1c6923",
+    "url": "/react-employee-directory/static/css/main.be5dd7fe.chunk.css"
+  },
+  {
+    "revision": "bc6e9119156d7a61ffdb",
+    "url": "/react-employee-directory/static/js/2.91f700d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.91f700d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01ae29b8ddb8de1c6923",
+    "url": "/react-employee-directory/static/js/main.ef36f511.chunk.js"
+  },
+  {
+    "revision": "af93e5f8d6168e3f2f99",
+    "url": "/react-employee-directory/static/js/runtime-main.2e31a536.js"
+  }
+]);
